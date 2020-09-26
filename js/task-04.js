bc51e2151fa238e455986edb512d21377207f26f
Liubomyr <ljubik@ukr.net>
2020-09-26 17:21:42 +0300

    make task 5 complete

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,4 @@
-// Завдання 3
+// Завдання 4
 // Задача 1-4
 // Использование if else и арифметических операторов
 // Напиши скрипт имитирующий вывод сообщений о списании средств на покупку товара.
@@ -53,3 +53,21 @@ console.log(message)
 //если orderPieces равно 10
 // то значение message будет равно
 // 'Недостаточно средств на счету!'
+
+// Это все к заданию  4 относится.let credits = 23580;
+// const pricePerDroid = 3000;
+// let droidAmount = prompt("Сколько дроидов желаете купить?");
+// droidAmount = Number(droidAmount);
+// const totalPrice = droidAmount * 3000;
+// if (droidAmount < 1) {
+//   console.log("Отменено пользователем!");
+// } else {
+//   if (credits < totalPrice) {
+//     const message = `Недостаточно средств на счету!`;
+//     console.log(message);
+//   } else {
+//     credits = credits - totalPrice;
+//     const message = `Вы купили ${droidAmount} дроидов, на счету осталось ${credits} кредитов.`;
+//     console.log(message);
+//   }
+// } (edited)
