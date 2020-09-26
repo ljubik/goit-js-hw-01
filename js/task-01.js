bb5861825a8eac110e860419d5da3098bad33a97
Liubomyr <ljubik@ukr.net>
2020-09-26 16:01:39 +0300

    complete 1-3 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -15,3 +15,16 @@ console.log(messageStock)
 priceOfGoods = 2000
 messageStock = `'Обрано «${nameOfGoods}», ціна за штуку ${priceOfGoods} кредитів'`
 console.log(messageStock)
+
+// здано:
+// const name = '«Генератор защитного поля»'; // Write code on this line
+// let price = 1000; // Write code on this line
+// const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+// price = 2000;
+// const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+// console.log(firstMessage);
+// // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+// console.log(secondMessage);
+// // 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
